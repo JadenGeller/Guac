a92cd87ba636640db065c996c696f65151b2fc7f
Jaden Geller <jaden.geller@gmail.com>
2017-05-05 02:23:48 -0700

    Updated package info

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
 setup(name='guac',
-    version='0.0.1',
+    version='1.0.0',
     description='Monadic do-notation in Python',
     url='https://github.com/JadenGeller/Guac',
     author='Jaden Geller',
     license='MIT',
     packages=['guac'],
+    classifiers=['Intended Audience :: Developers',
+                 'Intended Audience :: Education'
+                 'Topic :: Software Development :: Libraries',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Programming Language :: Python :: Implementation :: PyPy'
+                 ],
+    keywords='monad monadic coroutine generator pypy backtracking',
     zip_safe=True)
